b3206d6869f8fd375920ceb9f78e5cc78381e65d
Vitaliy Bull <14792518+senior-bull@users.noreply.github.com>
2021-12-02 19:35:10 +0300

    Tuple test

--- src/main/java/com/bull/examples/basic/Tuples.java
+++ src/main/java/com/bull/examples/basic/Tuples.java
@@ -1,8 +1,13 @@
 package com.bull.examples.basic;
 
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+
 public class Tuples {
 
     public static void main(String[] args) {
 
+        Tuple2<String, Integer> java8 = Tuple.of("Java", 8);
+        System.out.println(java8);
     }
 }
