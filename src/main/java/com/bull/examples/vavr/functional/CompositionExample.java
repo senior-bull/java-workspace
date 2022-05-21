0667edfa4358cde19af73d27f9cd0fbc1af981d5
Vitaliy Bykov <14792518+senior-bull@users.noreply.github.com>
2022-05-21 21:23:01 +0300

    Composition example

--- src/main/java/com/bull/examples/vavr/functional/CompositionExample.java
+++ src/main/java/com/bull/examples/vavr/functional/CompositionExample.java
@@ -1,2 +1,17 @@
-package com.bull.examples.vavr.functional;public class CompositionExample {
+package com.bull.examples.vavr.functional;
+
+import io.vavr.Function1;
+
+public class CompositionExample {
+
+    public static void main(String[] args) {
+
+        Function1<Integer, Integer> plusOne = a -> a + 1;
+        Function1<Integer, Integer> multiplyByTwo = a -> a * 2;
+
+        Function1<Integer, Integer> composed = plusOne.andThen(multiplyByTwo);
+
+        System.out.println(composed.apply(5));
+        System.out.println(composed.apply(10));
+    }
 }
