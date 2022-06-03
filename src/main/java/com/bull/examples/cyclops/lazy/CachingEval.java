78c47ece0943ab0d05a21e664f96e439a376cc43
Vitaliy Bykov <14792518+senior-bull@users.noreply.github.com>
2022-06-03 11:05:23 +0300

    Cyclops eval

--- src/main/java/com/bull/examples/cyclops/lazy/CachingEval.java
+++ src/main/java/com/bull/examples/cyclops/lazy/CachingEval.java
@@ -1,2 +1,22 @@
-package com.bull.examples.cyclops.lazy;public class CachingEval {
+package com.bull.examples.cyclops.lazy;
+
+import cyclops.control.Eval;
+
+public class CachingEval {
+
+    private static Eval<String> data = Eval.later(() -> {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "ABC";
+    });
+
+    public static void main(String[] args) {
+
+        System.out.println(data.get());
+        System.out.println(data.get());
+        System.out.println(data.get());
+    }
 }
