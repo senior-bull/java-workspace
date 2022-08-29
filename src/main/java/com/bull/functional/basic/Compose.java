111461a6ff8e6106372eacd3e5bc23414f29a9b1
Vitaliy Bykov <14792518+senior-bull@users.noreply.github.com>
2022-08-29 11:15:03 +0300

    Compose test

---
+++ src/main/java/com/bull/functional/basic/Compose.java
@@ -0,0 +1,23 @@
+package com.bull.functional.basic;
+
+import java.util.function.Function;
+
+public class Compose {
+
+    static Function<Integer, Integer> compose(Function<Integer, Integer> f1,
+                                              Function<Integer, Integer> f2) {
+        return new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer arg) {
+                return f1.apply(f2.apply(arg));
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(
+            compose(x -> x * x, x -> x + 2).apply(5)
+        );
+    }
+}
