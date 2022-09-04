05f1479b721984d63b497491da4b21c57943f812
Vitaliy Bykov <14792518+senior-bull@users.noreply.github.com>
2022-09-04 12:05:14 +0300

    more functional approach

--- src/main/java/com/bull/functional/examples/EmailValidation.java
+++ src/main/java/com/bull/functional/examples/EmailValidation.java
@@ -8,17 +8,14 @@ public class EmailValidation {
     static Pattern emailPattern =
         Pattern.compile("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
 
-    static Function<String, Result> emailChecker = s -> {
-        if (s == null) {
-            return new Result.Failure("email must not be null");
-        } else if (s.length() == 0) {
-            return new Result.Failure("email must not be empty");
-        } else if (emailPattern.matcher(s).matches()) {
-            return new Result.Success();
-        } else {
-            return new Result.Failure("email " + s + " is invalid.");
-        }
-    };
+    static Function<String, Result> emailChecker = s ->
+        s == null
+            ? new Result.Failure("email must not be null")
+            : s.length() == 0
+                ? new Result.Failure("email must not be empty")
+                : emailPattern.matcher(s).matches()
+                    ? new Result.Success()
+                    : new Result.Failure("email " + s + " is invalid.");
 
     public static void main(String... args) {
         validate("this.is@my.email");
@@ -32,12 +29,10 @@ public class EmailValidation {
     private static void sendVerificationMail(String s) {
         System.out.println("Mail sent to " + s);
     }
-    static void validate(String s) {
+    static Executable validate(String s) {
         Result result = emailChecker.apply(s);
-        if (result instanceof Result.Success) {
-            sendVerificationMail(s);
-        } else {
-            logError(((Result.Failure) result).getMessage());
-        }
+        return (result instanceof Result.Success)
+            ? () -> sendVerificationMail(s)
+            : () -> logError(((Result.Failure) result).getMessage());
     }
 }
---
+++ src/main/java/com/bull/functional/examples/Executable.java
@@ -0,0 +1,5 @@
+package com.bull.functional.examples;
+
+public interface Executable {
+    void exec();
+}
