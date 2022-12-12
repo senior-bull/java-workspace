60300420b18a9ec2c6b660c0c0e1e4e0e6d1ab6d
Vitaliy Bykov <14792518+senior-bull@users.noreply.github.com>
2022-12-12 13:05:05 +0300

    pattern matching test

--- src/main/java/com/bull/features/pmatching/NewSwitchCaseTest.java
+++ src/main/java/com/bull/features/pmatching/NewSwitchCaseTest.java
@@ -1,2 +1,26 @@
-package com.bull.features.pmatching;public class NewSwitchCaseTest {
+package com.bull.features.pmatching;
+
+import java.util.Arrays;
+
+public class NewSwitchCaseTest {
+
+    static String smartToString(Object o) {
+        return switch (o) {
+            case null -> "null";
+            case String s -> s;
+            case int[] ia -> Arrays.toString(ia);
+            default -> o.toString();
+        };
+    }
+
+    private static class X {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(smartToString(null));
+        System.out.println(smartToString("ABASDASD"));
+        System.out.println(smartToString(new int[] {1, 2, 3}));
+        System.out.println(smartToString(new X()));
+    }
 }
