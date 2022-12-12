5354ce23608bbf962df5fd79647e16dc87931c08
Vitaliy Bykov <14792518+senior-bull@users.noreply.github.com>
2022-12-12 12:13:05 +0300

    move

---
+++ src/main/java/com/bull/features/pmatching/OptionalTest.java
@@ -0,0 +1,28 @@
+/*
+package com.bull.features.pmatching;
+
+// https://www.infoq.com/articles/data-oriented-programming-java/
+
+sealed interface Opt<T> {
+    record Some<T>(T value) implements Opt<T> { }
+    record None<T>() implements Opt<T> { }
+}
+
+public class OptionalTest {
+
+    public static Opt<String> find(int i) {
+        if (i >= 0) {
+            return new Opt.Some<>("" + i);
+        } else {
+            return new Opt.None<>();
+        }
+    }
+
+    public static void main(String[] args) {
+        var result = find(5);
+        var s = switch (result) {
+            case Opt.Some(var value) -> 5;
+            case Opt.None() -> 6;
+        };
+    }
+}*/
