332b0f0bc0470bdb9e5fe4054f5841ed1634ca8c
Vitaliy Bykov <14792518+senior-bull@users.noreply.github.com>
2022-12-20 11:51:14 +0300

    new features

--- src/main/java/com/bull/features/instancof/RecordsInstanceof.java
+++ src/main/java/com/bull/features/instancof/RecordsInstanceof.java
@@ -4,12 +4,18 @@ public class RecordsInstanceof {
 
     record Point(int x, int y) {}
 
+    record Circle(Point center, int radius) {}
+
     public static void main(String[] args) {
         Object obj = new Point(2, 5);
         if (obj instanceof Point(int x, int y) p) {
-            System.out.println(p);
-            System.out.println(p.x);
-            System.out.println(p.y);
+            System.out.println(x);
+            System.out.println(y);
+        }
+
+        Object obj2 = new Circle(new Point(5, 6), 2);
+        if (obj2 instanceof Circle(Point(int x, int y), int rad)) {
+            System.out.println(rad);
         }
     }
 }
